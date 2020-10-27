2d7671ee4ab7b7c5e2902daa89b549a53b733d6f
Zack <zackaryeaton@gmail.com>
2020-10-27 10:32:13 -0400

    Update Work.js

--- src/sections/Work.js
+++ src/sections/Work.js
@@ -37,12 +37,13 @@ const Work = () => {
     <section id="work">
       <Heading icon={MdWork} title="Work" />
 
-      {/* <div className="bg-primary-800 text-white rounded p-6 mb-8 lg:mr-12">
-        I was unexpectedly let go from my previous role with my past employer after almost two years because of the ongoing economic turmoil
-        due to the COVID19 Pandemic. So if you or your company is hiring, please
+      <div className="bg-primary-800 text-white rounded p-6 mb-8 lg:mr-12">
+        My previous role at DC Dev Shop was a 3 month contract-to-hire. 
+        Due to the COVID-19 pandemic, my contract was not renewed.
+        So if you or your company is hiring, please
         contact me at{" "}
         <a href="mailto:zackeaton@gmail.com">zackeaton@gmail.com</a>.
-      </div> */}
+      </div>
 
       <div className="flex">
         <div className="w-1 bg-gray-500 rounded-full md:ml-6 opacity-25" />
